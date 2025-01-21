4e9750c6085d2bc9244660ca79e777356206e57e
Zyttal <zharlaw@gmail.com>
2025-01-21 20:31:00 +0800

    Add Final Sections, Contacts - Practically Done, but I have yet to make this a Responsive Site, will have to do that later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { AboutSection } from "@/components/sections/AboutSection";
 import { HeroSection } from "@/components/sections/HeroSection";
 import ExperienceSection from "@/components/sections/ExperienceSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
+import ContactSection from "@/components/sections/ContactSection";
 
 export default function Home() {
 
@@ -19,6 +20,7 @@ export default function Home() {
       <AboutSection/>
       <ExperienceSection/>
       <ProjectsSection/>
+      <ContactSection/>
     </main>
   );
 }
